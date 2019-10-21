6b27c8e2ddd9a4172db8f601cb0d5b3bb1609f8d
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2019-10-22 01:57:20 +0200

    promise api

---
+++ promise-api.js
@@ -0,0 +1,19 @@
+/// using this is usefull when creating unit test 
+/// because we put senarios for the test
+
+
+//// create Promise that already resolved
+// const p =Promise.resolve({id:1});
+// p.then(result => console.log(result));
+
+
+
+///// Create Promise that already rejected
+// const p =Promise.reject(new Error('reason for rejection....'));
+// p.catch(err => console.log(err.message));
+
+
+
+
+
+
