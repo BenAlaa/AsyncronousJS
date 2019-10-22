456785dd8bd967fc0efca7871c4f07bf1baf77d8
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2019-10-22 02:04:05 +0200

    parallel promises

--- promise-api.js
+++ promise-api.js
@@ -15,5 +15,36 @@
 
 
 
+////////////// ------ Parallel Promises ------------ ///////////////
+/// when you call multiple async operations on the same time
+/// and after they are ready you need to do something
 
 
+const p1 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        console.log('Async operation 1.....');
+        resolve(1);
+        reject(new Error('Operation 1 Rejected ....'));
+    },2000);
+});
+
+const p2 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        console.log('Async operation 2.....');
+        resolve(2);
+        reject(new Error('Operation 2 Rejected ....'));
+    },2000);
+});
+
+Promise.all([p1, p2])
+    .then(result => console.log(result))
+    .catch(err => console.log(err.message));
+
+
+/// note that if one operation is rejected the whole process will be rejected
+
+
+// if you need to do something as soon as the first operation complete
+// Promise.race([p1, p2])
+//     .then(result => console.log(result))
+//     .catch(err => console.log(err.message));
